ef5f309a7d30d54b1e265828b062241404049425
Juha Ruotsalainen <juha.ruotsalainen@ambientia.fi>
2022-10-25 22:06:06 +0300

    Added content from filesamples.com. For now I'm just happy to be able to scan all test files from resources/test.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,23 @@
+use std::path::PathBuf;
 use std::sync::mpsc;
 use std::sync::mpsc::Receiver;
 use std::thread;
+use walkdir::WalkDir;
 
-const VECTOR_SIZE: u32 = 10;
-
-pub fn start_walking() -> Receiver<String> {
+pub fn start_walking(first_step: &PathBuf) -> Receiver<String> {
     let (tx, rx) = mpsc::channel();
 
+    let starter = first_step.clone();
     thread::spawn(move || {
-        for n in 1..(VECTOR_SIZE + 1) {
-            let sendable = format!("Iteration #{}", n);
-            tx.send(sendable).unwrap();
+        let walker = WalkDir::new(starter).follow_links(true).into_iter();
+        for entry_result in walker {
+            if let Ok(entry) = entry_result {
+                if entry.file_type().is_file() {
+                    if let Some(path) = entry.path().to_str() {
+                        tx.send(path.to_string()).unwrap();
+                    }
+                }
+            }
         }
     });
     return rx;
@@ -19,16 +26,22 @@ pub fn start_walking() -> Receiver<String> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::env;
 
     #[test]
-    fn it_works() {
+    fn it_finds_the_expected_amount_of_files() {
+        let mut resource_dir = PathBuf::new();
+        if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+            resource_dir.push(manifest_dir);
+        }
+        resource_dir.push("resources");
+        resource_dir.push("test");
         let mut items: Vec<String> = vec![];
-        let rx = start_walking();
+        let rx = start_walking(&resource_dir);
         for received in rx {
             items.push(received);
         }
-        println!(">>> Got {} messages", items.len());
-        let expected = VECTOR_SIZE as usize;
-        assert_eq!(items.len(), expected);
+        // Real amount is 8 media files, but for now we accept the one Markdown file as well.
+        assert_eq!(items.len(), 9);
     }
 }
